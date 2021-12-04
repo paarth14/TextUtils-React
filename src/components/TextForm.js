c85bbf311edd31b07ab473e94ad5f396fc1e0328
Paarth Kothari <paarthkothari14@gmail.com>
2021-12-04 10:25:33 +0530

    Fixed issues (Word counting error and copy to clipboard navigator API

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,10 +28,7 @@ export default function TextForm(props) {
 
 
     const handleCopy = () => {
-        console.log("I am copy");
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         document.getSelection().removeAllRanges();
         props.showAlert("Copied to Clipboard!", "success");
     }
@@ -61,7 +58,7 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
-            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
             <p>{0.008 *  text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
